2da9eecdbfcf8e1b65affc8904e8ce7f043f3413
Max <coding@maax.gr>
2019-06-07 09:57:26 +0200

    first commit

---
+++ app/src/main/java/de/hofuniversity/mchackathon19/dronecontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package de.hofuniversity.mchackathon19.dronecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
